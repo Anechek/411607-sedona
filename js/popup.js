92954ae31e1698b0011d07d9d6d4c667bb361a01
Anechek <anechek1@yandex.ru>
2018-03-30 22:04:10 +0300

    Выполнила задание Оживление интерактивных элементов в личном проекте

---
+++ js/popup.js
@@ -0,0 +1,26 @@
+'use strict';
+(function () {
+  var onSearchButtonClick = function (evt) {
+    evt.preventDefault();
+    popupElement.classList.toggle('search-hotels-show');
+  };
+
+  var onFindButtonSubmit = function (evt) {
+    if (!inputElements[0].value || !inputElements[1].value || !inputElements[2].value || !inputElements[3].value) {
+      evt.preventDefault();
+      popupElement.classList.add('search-hotels-error');
+      setTimeout(function () {
+        popupElement.classList.remove('search-hotels-error');  
+      }, 1000);
+    }
+  };
+    
+  var newArray;
+  var popupElement = document.querySelector('.search-hotels');
+  var inputElements = popupElement.querySelectorAll('input');
+  var searchButtonElement = document.querySelector('.button-search');
+  searchButtonElement.addEventListener('click', onSearchButtonClick);
+  var findButtonElement = document.querySelector('.search-hotels');
+  findButtonElement.addEventListener('submit', onFindButtonSubmit);
+    
+})();
