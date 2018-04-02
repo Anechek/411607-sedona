522bde64396e4927becb29ba8292208b31ba3f97
Anechek <anechek1@yandex.ru>
2018-04-02 17:03:35 +0300

    Внесла исправления после первой проверки

--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,26 @@
 'use strict';
 (function () {
-  var onSearchButtonClick = function (evt) {
-    evt.preventDefault();
-    popupElement.classList.toggle('search-hotels-show');
-  };
+    var onSearchButtonClick = function (evt) {
+        evt.preventDefault();
+        popupElement.classList.toggle('search-hotels-show');
+    };
 
-  var onFindButtonSubmit = function (evt) {
-    if (!inputElements[0].value || !inputElements[1].value || !inputElements[2].value || !inputElements[3].value) {
-      evt.preventDefault();
-      popupElement.classList.add('search-hotels-error');
-      setTimeout(function () {
-        popupElement.classList.remove('search-hotels-error');  
-      }, 1000);
-    }
-  };
+    var onFormSubmit = function (evt) {
+        if (!inputElements[0].value || !inputElements[1].value || !inputElements[2].value || !inputElements[3].value) {
+            evt.preventDefault();
+            popupElement.classList.add('search-hotels-error');
+            setTimeout(function () {
+                popupElement.classList.remove('search-hotels-error');
+            }, 1000);
+        }
+    };
     
-  var newArray;
-  var popupElement = document.querySelector('.search-hotels');
-  var inputElements = popupElement.querySelectorAll('input');
-  var searchButtonElement = document.querySelector('.button-search');
-  searchButtonElement.addEventListener('click', onSearchButtonClick);
-  var findButtonElement = document.querySelector('.search-hotels');
-  findButtonElement.addEventListener('submit', onFindButtonSubmit);
+    var popupElement = document.querySelector('.search-hotels');
+    var inputElements = popupElement.querySelectorAll('input');
+    var searchButtonElement = document.querySelector('.button-search');
+    searchButtonElement.addEventListener('click', onSearchButtonClick);
+    var formElement = document.querySelector('.search-hotels');
+    formElement.addEventListener('submit', onFormSubmit);
+    popupElement.classList.remove('search-hotels-show');
     
 })();
